48006d634f9664ed7a55b36c20defdcf68e9112e
Piotr Tutak <p.tutak@outlook.com>
2018-04-30 16:00:36 +0200

    profile

--- iereact/src/components/Profile.js
+++ iereact/src/components/Profile.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import requestJSON from '../services/requestJSON';
-import {Button, Table, ListGroup, ListGroupItem, Image} from 'react-bootstrap';
+import {Button, Table, ListGroup, ListGroupItem, Image,Form, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 
 export default class Profile extends Component{
     constructor(props){
         super(props);
         this.state={
-            editable:false,
             profile:null,
+            newPass:'',
+            repNewPass:'',
             nameEdit:false,
-            passEdit:false,
+            passChange:false,
             picEdit:false
         };
         this.getProfile=this.getProfile.bind(this);
@@ -18,6 +19,19 @@ export default class Profile extends Component{
         this.renderName=this.renderName.bind(this);
         this.renderPassword=this.renderPassword.bind(this);
         this.renderPicture=this.renderPicture.bind(this);
+
+        this.handleEditName=this.handleEditName.bind(this);
+        this.getEditNameValidState=this.getEditNameValidState.bind(this);
+        this.handleEditNameChange=this.handleEditNameChange.bind(this);
+        this.handleUpdateName=this.handleUpdateName.bind(this);
+
+        this.handleChangePassword=this.handleChangePassword.bind(this);
+        this.getNewPassValidState=this.getNewPassValidState.bind(this);
+        this.getRepPassValidState=this.getRepPassValidState.bind(this);
+        this.handleNewPassChange=this.handleNewPassChange.bind(this);
+        this.handleRepPassChange=this.handleRepPassChange.bind(this);
+        this.handleUpdatePassword=this.handleUpdatePassword.bind(this);
+
     }
 
     getProfile(){
@@ -28,20 +42,105 @@ export default class Profile extends Component{
             })
     }
 
-    renderName(){
-        if (!this.state.nameEdit)
-            return <ListGroupItem>
-                Name: {this.state.profile.name}
-                <Button style={{position:'absolute', right:'100px', bottom:'3px' }}>Edit Name</Button>
-            </ListGroupItem>;
+    handleEditName(event){
+        event.preventDefault();
+        this.setState({nameEdit:true})
+    }
+
+    getEditNameValidState(){
         return null;
     }
+
+    handleEditNameChange(event){
+        let profile=this.state.profile;
+        profile.name=event.target.value;
+        this.setState({profile:profile});
+        event.preventDefault();
+
+    }
+
+    handleUpdateName(event){
+        this.setState({nameEdit:false});
+        event.preventDefault();
+    }
+
+    renderName(){
+        if (!this.state.nameEdit)
+            return (
+                <ListGroupItem>
+                    Name: {this.state.profile.name}
+                    <Button style={{position:'absolute', right:'100px', bottom:'3px' }} onClick={this.handleEditName}>Edit Name</Button>
+                </ListGroupItem>
+            );
+
+        return (
+            <ListGroupItem>
+                <Form>
+                    <FormGroup controlId='nameInput' validationState={this.getEditNameValidState()}>
+                        <ControlLabel>Name:</ControlLabel>
+                        <FormControl type='text' value={this.state.profile.name} onChange={this.handleEditNameChange}/>
+                    </FormGroup>
+                    <Button type='submit' onClick={this.handleUpdateName}>Update Name</Button>
+                </Form>
+            </ListGroupItem>
+        )
+    }
+
+    handleChangePassword(event){
+        this.setState({passChange:true});
+        event.preventDefault();
+    }
+
+    getNewPassValidState(){
+        if (this.state.newPass.length>=6)
+            return null;
+        return 'error';
+    }
+
+    getRepPassValidState(){
+        if(this.state.newPass===this.state.repNewPass)
+            return null;
+        return 'error';
+    }
+
+    handleNewPassChange(event){
+        this.setState({newPass:event.target.value});
+        event.preventDefault();
+    }
+
+    handleRepPassChange(event){
+        this.setState({repNewPass:event.target.value});
+        event.preventDefault();
+    }
+
+    handleUpdatePassword(event){
+        
+        this.setState({passChange:false});
+        event.preventDefault();
+    }
+
     renderPassword(){
-        if (!this.state.passEdit)
-            return <ListGroupItem>
-                <Button>Change Password</Button>
-            </ListGroupItem>;
-        return null;
+        if (!this.state.passChange)
+            return (
+                <ListGroupItem>
+                    <Button onClick={this.handleChangePassword}>Change Password</Button>
+                </ListGroupItem>
+            );
+        return (
+            <ListGroupItem>
+                <Form inline>
+                    <FormGroup style={{paddingRight:'20px'}} controlId='newPassword' validationState={this.getNewPassValidState()}>
+                        <ControlLabel>New Password:</ControlLabel>
+                        <FormControl type='password' value={this.state.newPass} placeholder={'New Password'} onChange={this.handleNewPassChange}/>
+                    </FormGroup>
+                    <FormGroup style={{paddingRight:'20px'}} controlId={'repeatPassword'} validationState={this.getRepPassValidState()}>
+                        <ControlLabel>Repeat Password:</ControlLabel>
+                        <FormControl type={'password'} value={this.state.repNewPass} placeholder={'Repeated Password'} onChange={this.handleRepPassChange}/>
+                    </FormGroup>
+                    <Button type={'submit'} onClick={this.handleUpdatePassword}>Update Password</Button>
+                </Form>
+            </ListGroupItem>
+        )
     }
     renderPicture(){
         if (!this.state.picEdit)
